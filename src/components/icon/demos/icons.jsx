3180205dc2956f36eaa5964a5f27ea3b006dae25
Bios Sun <biossun@gmail.com>
2018-06-11 17:35:34 +0800

    components:icon: 图标组件

---
+++ src/components/icon/demos/icons.jsx
@@ -0,0 +1,24 @@
+import { Icon } from 'nami'
+
+function IconBox({ name }) {
+    return (
+        <li className="icon-box">
+            <Icon name={name} />
+            <span className="icon-box__name">{name}</span>
+        </li>
+    )
+}
+
+function IconBoxList({ children }) {
+    return <ul className="icon-box-list">{children}</ul>
+}
+
+render(
+    <IconBoxList>
+        <IconBox name="up" />
+        <IconBox name="down" />
+        <IconBox name="left" />
+        <IconBox name="right" />
+        <IconBox name="github" />
+    </IconBoxList>
+)
---
+++ src/components/icon/index.tsx
@@ -0,0 +1,48 @@
+import React, { PureComponent } from 'react'
+import classnames from 'classnames'
+import CommonProps from '@utils/common-props'
+
+import icons from './icons'
+
+import './styles'
+
+export interface IconProps extends CommonProps<Icon> {
+    /**
+     * 图标类型；
+     */
+    name?: string
+}
+
+/**
+ * @component
+ *
+ * @displayname 图标
+ * @group general
+ *
+ * @description
+ *
+ *     Icon 组件提供一组 SVG 格式的图标。
+ *
+ *     {@demo "./demos/icons.jsx"}
+ *
+ *     ## "SVG Icon" vs "Icon Font"
+ *
+ *     Icon 组件之所以选择提供 SVG 格式的图标，而非通常所使用的 "Icon Font"，
+ *     是因为我们认为「图标应当是一种图形，而非文字」。*（有关详细的区别，请参阅[这篇文章](https://css-tricks.com/icon-fonts-vs-svg/)）*
+ *
+ *     当然，"Icon Font" 也是有很多优点的，而最要紧的一点是："Icon Font" 的图标颜色默认即为文本颜色，
+ *     为在 SVG 图标中实现这一优点，我们为图标设置了 `fill: currentColor;` 样式，以使图标颜色与其父元素的文本颜色的保持一致。
+ *
+ */
+export default class Icon extends PureComponent<IconProps> {
+    render() {
+        const { name, className, style } = this.props
+        const classes = classnames('icon', className)
+
+        return (
+            <svg className={classes} width="16" height="16" viewBox="0 0 1024 1024" style={style}>
+                {icons[name]()}
+            </svg>
+        )
+    }
+}
