07a89400b8f7bcfce99188cbdcb45d253aeb3761
Bios Sun <biossun@gmail.com>
2018-06-11 17:34:10 +0800

    components: 提供组件库入口文件

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import './styles'
+
+export { default as Button } from './components/button'
+export * from './components/button'
+
+export { default as Icon } from './components/icon'
+export * from './components/icon'
+
+export { default as Navbar } from './components/navbar'
+export * from './components/navbar'
+
+export { default as Menu } from './components/menu'
+export * from './components/menu'
+
+export { default as Grid } from './components/grid'
+export * from './components/grid'
