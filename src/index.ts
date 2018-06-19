940df76b7db049eb78cf4d274e0243e9b7514174
Bios Sun <biossun@gmail.com>
2018-06-19 23:38:39 +0800

    Opt - 给 src/index.ts 中的导入代码添加一些分组注释；

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 import './styles'
 
+// general
+// -----------------------------------------------------------------------------
+
 export { default as Button } from './components/button'
 export * from './components/button'
 
 export { default as Icon } from './components/icon'
 export * from './components/icon'
 
+// navigation
+// -----------------------------------------------------------------------------
+
 export { default as Navbar } from './components/navbar'
 export * from './components/navbar'
 
 export { default as Menu } from './components/menu'
 export * from './components/menu'
+
+// layout
+// -----------------------------------------------------------------------------
 
 export { default as Grid } from './components/grid'
 export * from './components/grid'
