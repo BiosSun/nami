2f35246dee3c25cd3e5d55f59d7c78714b055c47
Bios Sun <biossun@gmail.com>
2018-07-18 17:50:30 +0800

    Fix - docs - 修复 MenuItemLink 组件中，没有将 Menu 组件传递过来的属性追加到 Menu.Item 上的问题；

--- docs/src/components/menu-item-link/index.tsx
+++ docs/src/components/menu-item-link/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { Link, Route } from 'react-router-dom'
 import { Menu } from 'nami'
 
-export default function MenuItemLink({ to, exact, ...rest }: { to: string; exact?: boolean }) {
+export default function MenuItemLink({
+    to,
+    exact,
+    children,
+    ...otherProps
+}: {
+    to: string
+    exact?: boolean
+    children: ReactNode
+}) {
     return (
         <Route
             path={to}
             exact={exact}
             children={({ match }) => (
-                <Menu.Item active={!!match}>
-                    <Link to={to} {...rest} />
+                <Menu.Item active={!!match} {...otherProps}>
+                    <Link to={to}>{children}</Link>
                 </Menu.Item>
             )}
         />
