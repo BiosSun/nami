e36f254e5f5af7137b134c0f7e3238e17652950b
Bios Sun <biossun@gmail.com>
2018-07-24 17:45:10 +0800

    Dev - 仅导出组件本身，而不再导出这些组件的 props ；
    计划之后使用 namespace 技术，将组件类同时作为命名空间导出其自身的 props 和 state 声明。（如果可以的话）

--- src/index.ts
+++ src/index.ts
@@ -4,52 +4,28 @@ import './styles'
 // -----------------------------------------------------------------------------
 
 export { default as Button } from './components/button'
-export * from './components/button'
-
 export { default as Icon } from './components/icon'
-export * from './components/icon'
-
 export { default as Popover } from './components/popover'
-export * from './components/popover'
 
 // navigation
 // -----------------------------------------------------------------------------
 
 export { default as Navbar } from './components/navbar'
-export * from './components/navbar'
-
 export { default as Menu } from './components/menu'
-export * from './components/menu'
 
 // layout
 // -----------------------------------------------------------------------------
 
 export { default as Linear } from './components/linear'
-export * from './components/linear'
-
 export { default as Grid } from './components/grid'
-export * from './components/grid'
-
 export { default as Divider } from './components/divider'
-export * from './components/divider'
-
 export { default as Space } from './components/space'
-export * from './components/space'
-
 export { default as FlexibleSpace } from './components/flexible-space'
-export * from './components/flexible-space'
 
 // form
 // -----------------------------------------------------------------------------
 
 export { default as TextBox } from './components/textbox'
-export * from './components/textbox'
-
 export { default as Select } from './components/select'
-export * from './components/select'
-
 export { default as CheckBox } from './components/checkbox'
-export * from './components/checkbox'
-
 export { default as Radio } from './components/radio'
-export * from './components/radio'
