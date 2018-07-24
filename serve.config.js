9ba16eca1201309ae27bff7f603f96b7c485ad04
Bios Sun <biossun@gmail.com>
2018-07-24 14:47:59 +0800

    Dev - 更新项目所依赖的模块；

--- serve.config.js
+++ serve.config.js
@@ -2,7 +2,6 @@ const history = require('connect-history-api-fallback')
 const convert = require('koa-connect')
 
 module.exports = {
-    config: require('./site/webpack.config')(),
     add(app) {
         app.use(convert(history({})))
     },
