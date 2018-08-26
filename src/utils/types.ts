6e5be52a585316543a0d33d4dac95d4c3a9f4ce5
Bios Sun <biossun@gmail.com>
2018-08-26 23:22:25 +0800

    ✨ Omit - 用于过滤掉某个类型中的一些属性；

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,6 +1,11 @@
 /**
+ * 用于过滤某个类型中的属性
+ */
+type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
+
+/**
  * 组件处理状态
  */
 type State = 'success' | 'warning' | 'danger'
 
-export { State }
+export { Omit, State }
