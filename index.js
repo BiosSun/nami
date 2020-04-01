0f963e7be41d736490aa4bd86dd088ded36457a8
Bios Sun <biossun@gmail.com>
2020-04-02 01:07:10 +0800

    Updates

--- index.js
+++ index.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8ffde0e0";
+/******/ 	var hotCurrentHash = "7abb4a66";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
