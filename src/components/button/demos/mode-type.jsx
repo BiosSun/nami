db6d39955d3fa745d61bb68b5760e6238065e823
Bios Sun <biossun@gmail.com>
2020-04-06 21:02:28 +0800

    ✅ Button - 简化 Demo 中的布局；

--- src/components/button/demos/mode-type.jsx
+++ src/components/button/demos/mode-type.jsx
@@ -1,153 +1,169 @@
-import { Button, Icon } from 'nami'
+import { Button, Icon, HLinear } from 'nami'
 
 render(
     <div>
-        <Button>Button</Button>
-        <Button type="primary">Button</Button>
-        <Button type="info">Button</Button>
-        <Button type="success">Button</Button>
-        <Button type="warning">Button</Button>
-        <Button type="danger">Button</Button>
+        <HLinear spacing>
+            <Button>Button</Button>
+            <Button type="primary">Button</Button>
+            <Button type="info">Button</Button>
+            <Button type="success">Button</Button>
+            <Button type="warning">Button</Button>
+            <Button type="danger">Button</Button>
+        </HLinear>
 
         <hr />
 
-        <Button round>Button</Button>
-        <Button round type="primary">
-            Button
-        </Button>
-        <Button round type="info">
-            Button
-        </Button>
-        <Button round type="success">
-            Button
-        </Button>
-        <Button round type="warning">
-            Button
-        </Button>
-        <Button round type="danger">
-            Button
-        </Button>
+        <HLinear spacing>
+            <Button round>Button</Button>
+            <Button round type="primary">
+                Button
+            </Button>
+            <Button round type="info">
+                Button
+            </Button>
+            <Button round type="success">
+                Button
+            </Button>
+            <Button round type="warning">
+                Button
+            </Button>
+            <Button round type="danger">
+                Button
+            </Button>
+        </HLinear>
 
         <hr />
 
-        <Button outline>Button</Button>
-        <Button outline type="primary">
-            Button
-        </Button>
-        <Button outline type="info">
-            Button
-        </Button>
-        <Button outline type="success">
-            Button
-        </Button>
-        <Button outline type="warning">
-            Button
-        </Button>
-        <Button outline type="danger">
-            Button
-        </Button>
+        <HLinear spacing>
+            <Button outline>Button</Button>
+            <Button outline type="primary">
+                Button
+            </Button>
+            <Button outline type="info">
+                Button
+            </Button>
+            <Button outline type="success">
+                Button
+            </Button>
+            <Button outline type="warning">
+                Button
+            </Button>
+            <Button outline type="danger">
+                Button
+            </Button>
+        </HLinear>
 
         <hr />
 
-        <Button outline round>
-            Button
-        </Button>
-        <Button outline round type="primary">
-            Button
-        </Button>
-        <Button outline round type="info">
-            Button
-        </Button>
-        <Button outline round type="success">
-            Button
-        </Button>
-        <Button outline round type="warning">
-            Button
-        </Button>
-        <Button outline round type="danger">
-            Button
-        </Button>
+        <HLinear spacing>
+            <Button outline round>
+                Button
+            </Button>
+            <Button outline round type="primary">
+                Button
+            </Button>
+            <Button outline round type="info">
+                Button
+            </Button>
+            <Button outline round type="success">
+                Button
+            </Button>
+            <Button outline round type="warning">
+                Button
+            </Button>
+            <Button outline round type="danger">
+                Button
+            </Button>
+        </HLinear>
 
         <hr />
 
-        <Button flat>Button</Button>
-        <Button flat type="primary">
-            Button
-        </Button>
-        <Button flat type="info">
-            Button
-        </Button>
-        <Button flat type="success">
-            Button
-        </Button>
-        <Button flat type="warning">
-            Button
-        </Button>
-        <Button flat type="danger">
-            Button
-        </Button>
+        <HLinear spacing>
+            <Button flat>Button</Button>
+            <Button flat type="primary">
+                Button
+            </Button>
+            <Button flat type="info">
+                Button
+            </Button>
+            <Button flat type="success">
+                Button
+            </Button>
+            <Button flat type="warning">
+                Button
+            </Button>
+            <Button flat type="danger">
+                Button
+            </Button>
+        </HLinear>
 
         <hr />
 
-        <Button circle>
-            <Icon name="up" />
-        </Button>
-        <Button circle type="primary">
-            <Icon name="up" />
-        </Button>
-        <Button circle type="info">
-            <Icon name="up" />
-        </Button>
-        <Button circle type="success">
-            <Icon name="up" />
-        </Button>
-        <Button circle type="warning">
-            <Icon name="up" />
-        </Button>
-        <Button circle type="danger">
-            <Icon name="up" />
-        </Button>
+        <HLinear spacing>
+            <Button circle>
+                <Icon name="up" />
+            </Button>
+            <Button circle type="primary">
+                <Icon name="up" />
+            </Button>
+            <Button circle type="info">
+                <Icon name="up" />
+            </Button>
+            <Button circle type="success">
+                <Icon name="up" />
+            </Button>
+            <Button circle type="warning">
+                <Icon name="up" />
+            </Button>
+            <Button circle type="danger">
+                <Icon name="up" />
+            </Button>
+        </HLinear>
 
         <hr />
 
-        <Button circle outline>
-            <Icon name="up" />
-        </Button>
-        <Button circle outline type="primary">
-            <Icon name="up" />
-        </Button>
-        <Button circle outline type="info">
-            <Icon name="up" />
-        </Button>
-        <Button circle outline type="success">
-            <Icon name="up" />
-        </Button>
-        <Button circle outline type="warning">
-            <Icon name="up" />
-        </Button>
-        <Button circle outline type="danger">
-            <Icon name="up" />
-        </Button>
+        <HLinear spacing>
+            <Button circle outline>
+                <Icon name="up" />
+            </Button>
+            <Button circle outline type="primary">
+                <Icon name="up" />
+            </Button>
+            <Button circle outline type="info">
+                <Icon name="up" />
+            </Button>
+            <Button circle outline type="success">
+                <Icon name="up" />
+            </Button>
+            <Button circle outline type="warning">
+                <Icon name="up" />
+            </Button>
+            <Button circle outline type="danger">
+                <Icon name="up" />
+            </Button>
+        </HLinear>
 
         <hr />
 
-        <Button circle flat>
-            <Icon name="up" />
-        </Button>
-        <Button circle flat type="primary">
-            <Icon name="up" />
-        </Button>
-        <Button circle flat type="info">
-            <Icon name="up" />
-        </Button>
-        <Button circle flat type="success">
-            <Icon name="up" />
-        </Button>
-        <Button circle flat type="warning">
-            <Icon name="up" />
-        </Button>
-        <Button circle flat type="danger">
-            <Icon name="up" />
-        </Button>
+        <HLinear spacing>
+            <Button circle flat>
+                <Icon name="up" />
+            </Button>
+            <Button circle flat type="primary">
+                <Icon name="up" />
+            </Button>
+            <Button circle flat type="info">
+                <Icon name="up" />
+            </Button>
+            <Button circle flat type="success">
+                <Icon name="up" />
+            </Button>
+            <Button circle flat type="warning">
+                <Icon name="up" />
+            </Button>
+            <Button circle flat type="danger">
+                <Icon name="up" />
+            </Button>
+        </HLinear>
     </div>
 )
